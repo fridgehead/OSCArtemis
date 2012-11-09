c0c9635131197d9051adc44becbfa7f5a1607d01
tom wyatt <tom@imakethin.gs>
2012-11-09 15:00:43 +0000

    added some comments, remove packet counts and removed old packet types as I dont care about them. Ship weapon hits now forwarded to OSC

--- osctest.py
+++ osctest.py
@@ -7,7 +7,6 @@ import math
 import simpleOSC
 
 globaloptions = None
-log = open("log.txt", "w")
 
 shipId = 0
 shipStats = {"shield": -1, "energy" : -1 ,"coordY" : -1, "coordX" : -1, "warp rate" : -1,"rotation rate" : -1, "impulse rate" : -1, "unknown" : -1, "unknown2" : -1, "rotation":-1, "frontshield" : -1, "rearshield" : -1, "weaponlock" : -1, "autobeams": -1, "speed": -1}
@@ -55,21 +54,25 @@ def decodePacket(bitStr, message, statsMap):
 
 
 
-def processPacket(message, num):
+def processPacket(message):
 	global shipId
+	#get rid of empty packets
 	if len(message) == 0:
 		return
+	#convert packet to ints rather than a massive string.
 	mess = [ord(p) for p in message]
 
-	#ok classify based mess[40:44] then length
-	# old base was 26
+	
+	#these four chars are the message type
 	messType = mess[16:20]
-		
+	#some sort of object update	
 	if  messType ==  [0xf9,0x3d,0x80,0x80]:
 		#next 4 bytes are bitfieldtype, <ship id,shipid> 0
 		if mess[20:24] == [0,0,0,0]:
+			#keep-alive packet
 			return
 		else:
+			#seems to be a bitfield, 1 is playership? 2 and 4 also seen
 			playerShip = mess[20]
 			if playerShip == 0x01:
 				sId = struct.unpack("h", message[21:23])
@@ -88,12 +91,14 @@ def processPacket(message, num):
 		pass
 
 	elif messType == [0xc4, 0xd2, 0x3f, 0xb8]:
+		#this is most likely not the format but it gives
+		#repeatable results
 		vals = struct.unpack("iiiiiiiiiii", message[24:])
 		if vals[2] == 1:
 			if vals[6] == shipId:
 				print "WE GOT HIT!"
+				simpleOSC.sendOSCMsg("/shiphit", [1])
 			print "Damage from %i to %i" %(vals[5:7])
-		print "DAMAGE? ", vals 
 
 
 	elif messType == [0x5f, 0xc3, 0x72, 0xd6]:
@@ -104,45 +109,45 @@ def processPacket(message, num):
 		#print "LULWUT: ", mess[24:]	
 		pass
 
-
+# LETS START THIS MESS
 inte = -1
 
 if len(sys.argv) < 2 :
+	print "please choose one of the following interfaces to listen on"
 	l = pcapy.findalldevs()
 	print l
 	exit()
 else:
 	inte = int(sys.argv[1])
  
-# list all the network devices
+#setup pcap
 l = pcapy.findalldevs()
 max_bytes = 1024
 promiscuous = False
 read_timeout = 100 # in milliseconds
-
 #pc = pcapy.open_offline("captures/fun.pcap")
 pc = pcapy.open_live(l[inte], max_bytes, promiscuous, read_timeout)
 pc.setfilter('tcp src port 2010') 
 
+#packet header string
 splitStr =  "".join([chr(0xef),chr(0xbe), chr(0xad), chr(0xde)])
-pktCount = 0
 
+#start the OSC client
 simpleOSC.initOSCClient("192.168.0.3", port=12000)
 
 
 # callback for received packets
 def recv_pkts(hdr, data):
-	  global pktCount  
 	  packet = dpkt.ethernet.Ethernet(data)
 	  
+	  #split the packet using the DEADBEEF header. TCP likes to bundle multiple packets together
 	  packets = packet.data.tcp.data.split(splitStr)
+	  #for reading from pcap files
 	  #packets = packet.data.split(splitStr)
 	  for p in packets:
-		  processPacket(p,pktCount)
-		  pktCount += 1
+		  processPacket(p)
  
 packet_limit = -1 # infinite
 pc.loop(packet_limit, recv_pkts) # capture packets
-log.close()
 
 
--- readme.txt
+++ readme.txt
@@ -4,4 +4,14 @@ attempt to bridge Artemis ship stats to OSC for special effects etc
 
 so far the protocol is half documented in the notes.txt file. The script rips updates from the network stream and sticks them in a dictionary
 
-Script require pcapy and python 2.5
+
+
+osctest.py forwards all tracked stats to an IP using OSC. This was tested with a processing sketch as the destination and found to be pretty decent at updating
+
+
+Both scripts require pcapy and python 2.5 (as pcapy doesnt seem to work with 2.6+). The OSC test requires pyOSC and SimpleOSC
+
+
+TODO:
+added weapon hits to the stream
+tidy!
